8b51d8f6bc225cc9485fdddb3703fcdb791947ad
TLikithaKrishna <147525813+TLikithaKrishna@users.noreply.github.com>
2023-10-10 20:59:16 +0530

    Add files via upload

---
+++ 6OCT2023/Lab003.py
@@ -0,0 +1,17 @@
+# Take a input from a user and print the table
+# How to use the print with formatting f
+# print(f"),How can print the same?
+#n=2 and print
+
+input =(input("Enter the number"))
+n = 2
+print(f"2 * 1 = {n* 1}")
+print(f"2 * 2 = {n* 2}")
+print(f"2 * 3 = {n* 3}")
+print(f"2 * 4 = {n* 4}")
+print(f"2 * 5 = {n* 5}")
+print(f"2 * 6 = {n* 6}")
+print(f"2 * 7 = {n* 7}")
+print(f"2 * 8 = {n* 8}")
+print(f"2 * 9 = {n* 9}")
+print(f"2 * 10 = {n* 10}")
---
+++ 6OCT2023/Lab004.py
@@ -0,0 +1,13 @@
+#List all the functions available for the String Data Type
+
+#capitalize() function
+#lower() function
+#title() function
+#upper() function
+#count() function
+#casefold() function
+#find() function
+#replace() function
+#join() function
+#swapcase() function
+
