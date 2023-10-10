6f955b56d47b1d43234137be652ea53cef75a838
TLikithaKrishna <147525813+TLikithaKrishna@users.noreply.github.com>
2023-10-10 20:13:04 +0530

    Add files via upload

---
+++ 4OCT2023/Lab002.py
@@ -0,0 +1,15 @@
+#Print the Name of the user by taking it from input command.
+Name = input("Enter your Name : ")
+print(Name)
+
+#Take first name and last name of user and print with sep="-" and end with/t
+firstname = input("Enter your firstname: ")
+lastname = input("Enter your lastname: ")
+print(firstname,lastname,sep='-',end='\t')
+
+#Take a user input as name and say with message
+#"You are welcome."<name>" to the python class."
+message = input("Enter your Name")
+print('You are welcome',message,'to the python class')
+
+
